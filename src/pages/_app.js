4c42c7e2acaab4014807c3f90acb2cd51df6e4ca
PaulHaze <dev@artemist.com.au>
2022-02-10 09:03:49 +1100

    _app.js finished for now

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,40 @@ import React from 'react';
 import Script from 'next/script';
 import Head from 'next/head';
 
-import { LoadingScreen } from '@Components';
+import { LoadingScreen, ScrollToTop } from '@Components';
 
 import '@/styles/main.scss';
 import '@/styles/custom.scss';
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Archo</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
+      <LoadingScreen />
+      <Component {...pageProps} />
+      <ScrollToTop />
+
+      {/* SCRIPTS */}
+      <Script id="wow" src="/assets/js/wow.min.js" />
+      <Script
+        id="splitting"
+        strategy="beforeInteractive"
+        src="/assets/js/splitting.min.js"
+      />
+
+      <Script id="simpleParallax" src="/assets/js/simpleParallax.min.js" />
+      <Script id="isotope" src="/assets/js/isotope.pkgd.min.js" />
+
+      <Script src="/assets/js/main.js" id="init" strategy="lazyOnload" />
+    </>
+  );
 }
 
 export default MyApp;
