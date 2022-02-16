fadcb281094666ca2478669935918f63c0e6e7f7
PaulHaze <dev@artemist.com.au>
2022-02-17 09:12:23 +1100

    update wow

---
+++ src/features/LightExampleOne/AboutUs/index.js
@@ -0,0 +1,7 @@
+export function AboutUs() {
+  return (
+    <div className="h-[200px] bg-white">
+      <p>AboutUs Placeholder</p>
+    </div>
+  );
+}
--- src/features/LightExampleOne/index.js
+++ src/features/LightExampleOne/index.js
@@ -2,12 +2,14 @@
 import { LightLayout } from '@/layouts';
 import { Footer } from '@/components';
 import { Hero } from './Hero';
+import { AboutUs } from './AboutUs';
 
 export function LightExampleOne() {
   return (
     <LightLayout>
       <div className="min-h-screen bg-primary-dark">
         <Hero />
+        <AboutUs />
       </div>
       <div className="mt-2 bg-primary-dark">
         <Footer />
