8eb33a11e60defefdf7b420028645fbf3a9e9f02
PaulHaze <dev@artemist.com.au>
2022-02-17 15:33:41 +1100

    removed old footer

--- src/features/AboutUs/AboutUs_1.js
+++ src/features/AboutUs/AboutUs_1.js
@@ -31,16 +31,16 @@ export function AboutUs() {
           {/* TEXT */}
           <div className="w-full mx-auto md:w-7/12 sm:max-w-xs md:max-w-lg ">
             <div className="exp-content md:mt-0 px-2 h-full flex flex-col justify-between">
-              <h6 className="sub-title wow fadeInUp" data-wow-delay=".6s">
+              <h6 className="sub-title wow fadeInUp" data-wow-delay=".4s">
                 About Us
               </h6>
-              <div className=" wow fadeInUp" data-wow-delay=".9s">
+              <div className=" wow fadeInUp" data-wow-delay=".8s">
                 <h2 className="mb-20 playfont">
                   {aboutUsData.title.first} <br /> {aboutUsData.title.second}
                 </h2>
                 <p>{aboutUsData.content}</p>
               </div>
-              <div className="numbers mt-0 wow fadeInUp" data-wow-delay="1.2s">
+              <div className="numbers mt-0 wow fadeInUp" data-wow-delay="1s">
                 <div className="row">
                   {aboutUsData.numbers.map(item => (
                     <div className="col-md-4" key={item.id}>
