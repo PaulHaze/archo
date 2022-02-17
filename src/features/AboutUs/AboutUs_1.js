9ad3d038ba9c1568dda2f5ed300f6125b750f11f
PaulHaze <dev@artemist.com.au>
2022-02-17 15:09:03 +1100

    about us intro section done

--- src/features/AboutUs/AboutUs_1.js
+++ src/features/AboutUs/AboutUs_1.js
@@ -27,17 +27,20 @@ export function AboutUs() {
               </div>
             </div>
           </div>
-          <div className="w-full mx-auto md:w-7/12 sm:max-w-xs">
-            <div
-              className="exp-content wow fadeInUp md:mt-0 pl-10"
-              data-wow-delay=".3s"
-            >
-              <h6 className="sub-title">About Us</h6>
-              <h2 className="mb-20 playfont">
-                {aboutUsData.title.first} <br /> {aboutUsData.title.second}
-              </h2>
-              <p>{aboutUsData.content}</p>
-              <div className="numbers mt-20">
+
+          {/* TEXT */}
+          <div className="w-full mx-auto md:w-7/12 sm:max-w-xs md:max-w-lg ">
+            <div className="exp-content md:mt-0 px-2 h-full flex flex-col justify-between">
+              <h6 className="sub-title wow fadeInUp" data-wow-delay=".6s">
+                About Us
+              </h6>
+              <div className=" wow fadeInUp" data-wow-delay=".9s">
+                <h2 className="mb-20 playfont">
+                  {aboutUsData.title.first} <br /> {aboutUsData.title.second}
+                </h2>
+                <p>{aboutUsData.content}</p>
+              </div>
+              <div className="numbers mt-0 wow fadeInUp" data-wow-delay="1.2s">
                 <div className="row">
                   {aboutUsData.numbers.map(item => (
                     <div className="col-md-4" key={item.id}>
