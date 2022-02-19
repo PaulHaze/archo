39972996c9502c3e3c365a63ef7432910ccbf846
PaulHaze <dev@artemist.com.au>
2022-02-19 20:51:17 +1100

    add split component back

--- components/Split/index.js
+++ components/Split/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 export class Split extends React.Component {
-  // target = React.createRef();
+  target = React.createRef();
 
-  // split = () => {
-  //   if (this.target.current) {
-  //     Splitting({ target: this.target.current });
-  //   }
-  // };
+  split = () => {
+    if (this.target.current) {
+      Splitting({ target: this.target.current });
+    }
+  };
 
-  // componentDidMount = this.split;
-  // componentDidUpdate = this.split;
+  componentDidMount = this.split;
+  componentDidUpdate = this.split;
 
   render() {
     const { children } = this.props;
-    return <div>{children}</div>;
-    // return <div ref={this.target}>{this.props.children}</div>;
+    // return <div>{children}</div>;
+    return <div ref={this.target}>{this.props.children}</div>;
   }
 }
