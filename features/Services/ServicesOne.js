d7065ee848dca7b272ab7466c2754a1a6a25bf85
PaulHaze <dev@artemist.com.au>
2022-02-22 10:41:23 +1100

    styled the header

--- features/Services/ServicesOne.js
+++ features/Services/ServicesOne.js
@@ -1,10 +1,16 @@
+import servicesData from '@/data/services_1.json';
 import { ServiceCard } from './ServiceCard';
 
 export function ServicesOne() {
   return (
     <div className="container bg-primary-light p-0">
-      <div className="header py-10 text-center">
-        <p>Services Placeholder</p>
+      <div className="section-head bb text-center py-20">
+        <h6 className="wow fadeInDown" data-wow-delay=".3s">
+          Best Features
+        </h6>
+        <h4 className="playfont wow flipInX" data-wow-delay=".5s">
+          Our Services
+        </h4>
       </div>
       <div className="cards space-y-2 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-4">
         <ServiceCard />
