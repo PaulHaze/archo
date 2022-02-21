9b39bf1e94e31dd71cdff78f9f7e55d84c47f1cf
PaulHaze <dev@artemist.com.au>
2022-02-22 09:42:19 +1100

    next config updated

--- next.config.js
+++ next.config.js
@@ -10,11 +10,6 @@ module.exports = {
     buildActivity: false,
   },
   eslint: {
-    ignoreDuringBuilds: false,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
