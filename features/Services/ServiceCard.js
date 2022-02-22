11ad0ac277cef08bf42e493e8f988caf23a37cad
PaulHaze <dev@artemist.com.au>
2022-02-22 12:05:42 +1100

    pre services scss refactor commit

--- features/Services/ServiceCard.js
+++ features/Services/ServiceCard.js
@@ -1,7 +1,22 @@
-export function ServiceCard() {
+import Link from 'next/link';
+
+export function ServiceCard({ id, title, content }) {
   return (
-    <div className="h-[280px] w-full bg-blue-200">
-      <p>ServiceCard Placeholder</p>
+    // <div className="item-bx">
+    //   <h2 className="numb">{id}</h2>
+    //   <h6 className="mb-20">{title}</h6>
+    //   <p>{content}</p>
+    //   <Link href="/about">
+    //     <a className="more mt-30">Read More</a>
+    //   </Link>
+    // </div>
+    <div className="col-lg-3 col-md-6 item-bx" key={id}>
+      <h2 className="numb">{id}</h2>
+      <h6 className="mb-20">{title}</h6>
+      <p>{content}</p>
+      <Link href="/about">
+        <a className="more mt-30">Read More</a>
+      </Link>
     </div>
   );
 }
--- features/Services/ServicesOne.js
+++ features/Services/ServicesOne.js
@@ -1,23 +1,63 @@
 import servicesData from '@/data/services_1.json';
 import { ServiceCard } from './ServiceCard';
 
+import Link from 'next/link';
+
 export function ServicesOne() {
+  const renderServices = servicesData.services.map(({ id, title, content }) => (
+    <ServiceCard key={id} id={id} title={title} content={content} />
+  ));
   return (
-    <div className="container bg-primary-light p-0">
-      <div className="section-head bb text-center py-20">
-        <h6 className="wow fadeInDown" data-wow-delay=".3s">
-          Best Features
-        </h6>
-        <h4 className="playfont wow flipInX" data-wow-delay=".5s">
-          Our Services
-        </h4>
-      </div>
-      <div className="cards space-y-2 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-4">
-        <ServiceCard />
-        <ServiceCard />
-        <ServiceCard />
-        <ServiceCard />
-      </div>
-    </div>
+    <>
+      <section className="container bg-primary-light p-0">
+        <div className="section-head text-center py-20">
+          <h6 className="wow fadeInDown" data-wow-delay=".3s">
+            Best Features
+          </h6>
+          <h4 className="playfont wow flipInX" data-wow-delay=".5s">
+            Our Services
+          </h4>
+        </div>
+        <div
+          className="row bord-box bg-img wow fadeInUp"
+          data-wow-delay=".3s"
+          style={{ backgroundImage: `url(${servicesData.image}` }}
+        >
+          {renderServices}
+        </div>
+      </section>
+      <h5 className="py-5">Theirs</h5>
+      <section className="services section-padding bg-gray">
+        <div className="container">
+          {/* HEADER */}
+          <div className="section-head text-center py-20">
+            <h6 className="wow fadeInDown" data-wow-delay=".3s">
+              Best Features
+            </h6>
+            <h4 className="playfont wow flipInX" data-wow-delay=".5s">
+              Our Services
+            </h4>
+          </div>
+
+          {/* CARDS */}
+          <div
+            className="row bord-box bg-img wow fadeInUp"
+            data-wow-delay=".3s"
+            style={{ backgroundImage: `url(${servicesData.image}` }}
+          >
+            {servicesData.services.map(item => (
+              <div className="col-lg-3 col-md-6 item-bx" key={item.id}>
+                <h2 className="numb">{item.id}</h2>
+                <h6 className="mb-20">{item.title}</h6>
+                <p>{item.content}</p>
+                <Link href="/about">
+                  <a className="more mt-30">Read More</a>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
