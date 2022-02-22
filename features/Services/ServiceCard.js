20934dabbb6f4a847bc165ca3002d683e8f03ee4
PaulHaze <dev@artemist.com.au>
2022-02-23 10:01:31 +1100

    finish services section

--- features/Services/ServiceCard.js
+++ features/Services/ServiceCard.js
@@ -2,20 +2,15 @@ import Link from 'next/link';
 
 export function ServiceCard({ id, title, content }) {
   return (
-    // <div className="item-bx">
-    //   <h2 className="numb">{id}</h2>
-    //   <h6 className="mb-20">{title}</h6>
-    //   <p>{content}</p>
-    //   <Link href="/about">
-    //     <a className="more mt-30">Read More</a>
-    //   </Link>
-    // </div>
-    <div className="col-lg-3 col-md-6 item-bx" key={id}>
-      <h2 className="numb">{id}</h2>
-      <h6 className="mb-20">{title}</h6>
+    <div
+      className="item-bx flex flex-col w-full p-3 md:flex-50 md:max-w-[50%]  lg:flex-25 lg:max-w-[25%] "
+      key={id}
+    >
+      <h2 className="numb my-5">{id}</h2>
+      <h6 className="mb-20 font-bold text-lg">{title}</h6>
       <p>{content}</p>
       <Link href="/about">
-        <a className="more mt-30">Read More</a>
+        <a className="more mt-30 mb-10">Read More</a>
       </Link>
     </div>
   );
--- features/Services/ServicesOne.js
+++ features/Services/ServicesOne.js
@@ -1,63 +1,28 @@
-import servicesData from '@/data/services_1.json';
 import { ServiceCard } from './ServiceCard';
-
-import Link from 'next/link';
+import servicesData from '@/data/services_1.json';
 
 export function ServicesOne() {
   const renderServices = servicesData.services.map(({ id, title, content }) => (
     <ServiceCard key={id} id={id} title={title} content={content} />
   ));
   return (
-    <>
-      <section className="container bg-primary-light p-0">
-        <div className="section-head text-center py-20">
-          <h6 className="wow fadeInDown" data-wow-delay=".3s">
-            Best Features
-          </h6>
-          <h4 className="playfont wow flipInX" data-wow-delay=".5s">
-            Our Services
-          </h4>
-        </div>
-        <div
-          className="row bord-box bg-img wow fadeInUp"
-          data-wow-delay=".3s"
-          style={{ backgroundImage: `url(${servicesData.image}` }}
-        >
-          {renderServices}
-        </div>
-      </section>
-      <h5 className="py-5">Theirs</h5>
-      <section className="services section-padding bg-gray">
-        <div className="container">
-          {/* HEADER */}
-          <div className="section-head text-center py-20">
-            <h6 className="wow fadeInDown" data-wow-delay=".3s">
-              Best Features
-            </h6>
-            <h4 className="playfont wow flipInX" data-wow-delay=".5s">
-              Our Services
-            </h4>
-          </div>
+    <section className="container services bg-primary-light p-0 pb-20 w-full">
+      <div className="section-head text-center py-20">
+        <h6 className="wow fadeInDown" data-wow-delay=".3s">
+          Best Features
+        </h6>
+        <h4 className="playfont wow flipInX" data-wow-delay=".5s">
+          Our Services
+        </h4>
+      </div>
 
-          {/* CARDS */}
-          <div
-            className="row bord-box bg-img wow fadeInUp"
-            data-wow-delay=".3s"
-            style={{ backgroundImage: `url(${servicesData.image}` }}
-          >
-            {servicesData.services.map(item => (
-              <div className="col-lg-3 col-md-6 item-bx" key={item.id}>
-                <h2 className="numb">{item.id}</h2>
-                <h6 className="mb-20">{item.title}</h6>
-                <p>{item.content}</p>
-                <Link href="/about">
-                  <a className="more mt-30">Read More</a>
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-    </>
+      <div
+        className="flex flex-wrap  bord-box bg-img wow fadeInUp max-w-lg mx-auto"
+        data-wow-delay=".3s"
+        style={{ backgroundImage: `url(${servicesData.image}` }}
+      >
+        {renderServices}
+      </div>
+    </section>
   );
 }
